98969ad4efbd328be4f8316475653114ede25ffc
Loïc Dessaules <loic.dessaules@heig-vd.ch>
2020-05-13 11:46:14 +0200

    Bad sm. code in wrong class, refa : Move order displaying in Order class

--- src/main/java/ch/heigvd/gen2019/Order.java
+++ src/main/java/ch/heigvd/gen2019/Order.java
@@ -26,4 +26,27 @@ public class Order {
     public void AddProduct(Product product) {
         products.add(product);
     }
+
+    @Override
+    public String toString() {
+        String res = "";
+
+        res += "{";
+        res += "\"id\": ";
+        res += getOrderId();
+        res += ", ";
+        res += "\"products\": [";
+        for (int j = 0; j < getProductsCount(); j++) {
+            res += getProduct(j);
+        }
+
+        if (getProductsCount() > 0) {
+            res = res.substring(0, res.length()-2);
+        }
+
+        res += "]";
+        res += "}, ";
+
+        return res;
+    }
 }
