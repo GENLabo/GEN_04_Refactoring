ea6c146afce280d2e02725aa9c717f3f5a64be28
vitorva <vitor.vazafonso@gmail.com>
2020-05-21 11:15:25 +0200

    Bad smell : divergent change, Refactor : extract classes

--- src/main/java/ch/heigvd/gen2019/OrdersWriter.java
+++ src/main/java/ch/heigvd/gen2019/OrdersWriter.java
@@ -1,39 +1,17 @@
 package ch.heigvd.gen2019;
 
+import ch.heigvd.gen2019.serializers.OrdersSerializer;
+
 public class OrdersWriter {
     private Orders orders;
+    private OrdersSerializer ordersSerializer;
 
-    public OrdersWriter(Orders orders) {
+    public OrdersWriter(Orders orders, OrdersSerializer ordersSerializer) {
         this.orders = orders;
+        this.ordersSerializer = ordersSerializer;
     }
 
     public String getContents() {
-        StringBuffer sb = new StringBuffer("{\"orders\": [");
-
-        for (int i = 0; i < orders.getOrdersCount(); i++) {
-            Order order = orders.getOrder(i);
-            sb.append("{");
-            sb.append("\"id\": ");
-            sb.append(order.getOrderId());
-            sb.append(", ");
-            sb.append("\"products\": [");
-            for (int j = 0; j < order.getProductsCount(); j++) {
-                order.getProduct(j).extractProductContents(sb);
-            }
-
-            if (order.getProductsCount() > 0) {
-                sb.delete(sb.length() - 2, sb.length());
-            }
-
-            sb.append("]");
-            sb.append("}, ");
-        }
-
-        if (orders.getOrdersCount() > 0) {
-            sb.delete(sb.length() - 2, sb.length());
-        }
-
-        return sb.append("]}").toString();
+        return ordersSerializer.toParse(orders);
     }
-
 }
